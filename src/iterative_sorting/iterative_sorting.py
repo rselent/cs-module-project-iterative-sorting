b76037eaa6591c375834a4def360c9c3a13ec49b
rselent <rselent@ufl.edu>
2020-05-18 18:56:33 -0400

    donezo + stretch

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -7,25 +7,46 @@ def selection_sort(arr):
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc)
         # Your code here
-
+        for n in range( i + 1, len( arr)):
+            if arr[ cur_index] > arr[ n]:
+                cur_index = n
 
         # TO-DO: swap
         # Your code here
-
+        arr[ i], arr[ cur_index] = arr[ cur_index], arr[ i]
     return arr
 
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort(arr):
     # Your code here
-
-
+    for a in range( len( arr) - 1):
+        for b in range( 0, len( arr) - a - 1):
+            if arr[b] > arr[b+1]:
+                arr[b], arr[b+1] = arr[b+1], arr[b]
     return arr
 
 
 # STRETCH: implement the Count Sort function below
 def count_sort(arr, maximum=-1):
     # Your code here
+    for i in range( len( arr)):     # find true maximum value
+        if arr[i] > maximum:
+            maximum = arr[i]
+    print( "max value", maximum)
 
+    n = 0
+    maxVal = maximum + 1
+    counter = [0] * maxVal
+    errorNegatives = f"Error, negative numbers not allowed in Count Sort"
 
+    for i in arr:
+        counter[i] += 1
+    for a in range( maxVal):
+        for b in range( counter[a]):
+            if arr[n] < 0:
+                return errorNegatives
+            else:
+                arr[n] = a
+                n += 1
     return arr
--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,14 +1,44 @@
 def linear_search(arr, target):
     # Your code here
-
-
+    for n in range( len( arr)):
+        if arr[n] == target:
+            return n
     return -1   # not found
 
 
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
-
     # Your code here
+    left = 0
+    right = len( arr) - 1
 
+    print( "left", left, "\nright", right)
+
+    while left <= right:
+        mid = (left + right) // 2
+        print( "\nmid", mid)
+
+        if arr[ mid] == target:
+            return mid
+        
+        if arr[ mid] < target:
+            left = mid + 1
+            print( "left", left)
+        else:
+            right = mid - 1
+            print( "right", right)
 
     return -1  # not found
+
+
+def main():
+    #testArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    #testArray = [-2, 7, 3, -9, 5, 1, 0, 4, -6]
+    testArray = [-9, -8, -6, -4, -3, -2, 0, 1, 2, 3, 5, 7, 8, 9]
+
+    print( binary_search( testArray, 7))
+    print( linear_search( testArray, -6))
+
+
+if __name__ == "__main__":
+    main()
